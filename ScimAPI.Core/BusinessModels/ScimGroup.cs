be82303d5553c6128a0a36b230bee3440099f4a0
olet88 <otobiesen@hotmail.com>
2025-03-08 23:22:22 +0100

    Fixed casing

--- ScimAPI.Core/BusinessModels/ScimGroup.cs
+++ ScimAPI.Core/BusinessModels/ScimGroup.cs
@@ -2,22 +2,22 @@
 {
     public class ScimGroup
     {
-        public List<string> Schemas { get; set; }
-        public string ExternalId { get; set; }
+        public List<string>? Schemas { get; set; }
+        public required string ExternalId { get; set; }
         public string? Id { get; set; }
-        public string DisplayName { get; set; }
+        public required string DisplayName { get; set; }
         public IEnumerable<ScimMember>? Members { get; set; }
         public ScimMeta? Meta { get; set; }
     }
 
     public class ScimMember
     {
-        public string value { get; set; }
+        public string Value { get; set; }
         public string? Ref { get; set; }
     }
 
     public class ScimMeta
     {
-        public string ResourceType { get; set; }
+        public string? ResourceType { get; set; }
     }
 }
