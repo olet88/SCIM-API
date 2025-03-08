6dfff1c0028a867472af1331605d3292cabb61fd
olet88 <otobiesen@hotmail.com>
2025-03-08 17:38:54 +0100

    gitignore

--- ScimLibrary/obj/Debug/net8.0/ScimAPI.AssemblyInfo.cs
+++ ScimLibrary/obj/Debug/net8.0/ScimAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScimAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4956216ff158d85de1ef027e33b406ba1af0afef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a511f5d4ed02232903350befe1393585650ae4c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScimAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScimAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
