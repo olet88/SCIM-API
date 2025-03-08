10c93be4fad00fc08befd6bb04439b60c8fbf4aa
olet88 <otobiesen@hotmail.com>
2025-03-08 19:20:11 +0100

    Cleanup

---
+++ ScimLibrary/Repositories/IScimGroupRepository.cs
@@ -0,0 +1,17 @@
+﻿using ScimLibrary.BusinessModels;
+using ScimLibrary.Models;
+using System.Collections.Concurrent;
+
+namespace ScimAPI.Repository
+{
+    public interface IScimGroupRepository
+    {
+        Task<ScimGroup> GetGroupByIdAsync(string id);
+        Task<IEnumerable<ScimGroup>> GetAllGroupsAsync();
+        Task AddGroupAsync(ScimGroup scimGroup);
+        Task UpdateGroupAsync(ScimGroup scimGroup);
+        Task ReplaceGroupAsync(ScimGroup scimGroup);
+        Task DeleteGroupAsync(string id);
+    }
+
+}
--- ScimLibrary/Repositories/IScimUserRepository.cs
+++ ScimLibrary/Repositories/IScimUserRepository.cs
@@ -13,15 +13,4 @@ namespace ScimAPI.Repository
         Task ReplaceUserAsync(ScimUser scimUser);
         Task DeleteUserAsync(string userId);
     }
-
-    public interface IScimGroupRepository
-    {
-        Task<ScimGroup> GetGroupByIdAsync(string id);
-        Task<IEnumerable<ScimGroup>> GetAllGroupsAsync();
-        Task AddGroupAsync(ScimGroup scimGroup);
-        Task UpdateGroupAsync(ScimGroup scimGroup);
-        Task ReplaceGroupAsync(ScimGroup scimGroup);
-        Task DeleteGroupAsync(string id);
-    }
-
 }
